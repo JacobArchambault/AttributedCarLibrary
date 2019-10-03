d1c34e97cb34aa09aa996b364fae3bc8b687062c
Jacob Archambault <jacob.archambault@waystar.com>
2019-10-03 09:01:55 -0400

    Add project files.

---
+++ Motorcycle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributedCarLibrary
+{
+    [Serializable, VehicleDescription(Description = "My rocking Harley")]
+    public class Motorcycle
+    {
+    }
+}
---
+++ VehicleDescriptionAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: CLSCompliant(true)]
+namespace AttributedCarLibrary
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false)]
+    public sealed class VehicleDescriptionAttribute : Attribute
+    {
+        public string Description { get; set; }
+        public VehicleDescriptionAttribute(string vehicleDescription) => Description = vehicleDescription;
+        public VehicleDescriptionAttribute() { }
+    }
+}
